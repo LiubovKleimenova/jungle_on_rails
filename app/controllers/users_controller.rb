ad590b8fe9edbf48a33ac54f44a6f00d90010084
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-04-02 18:05:36 -0400

    added actions to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+  def new
+    end
+
+    def create
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -73,10 +73,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  # I've created a gif controller so I have a page I can secure later. 
-  # This is optional (as is the root to: above).
-  get '/cool' => 'gif#cool'
-  get '/sweet' => 'gif#sweet'
   
   # These routes will be for signup. The first renders a form in the browse, the second will 
   # receive the form and create a user in our database using the data given to us by the user.
