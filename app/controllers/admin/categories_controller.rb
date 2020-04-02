78fc58eadffd9eca8cd6447f77e529e731e3d4a9
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-04-02 16:33:19 -0400

    created erb templates for creating new category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,7 +3,7 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV['username'], password: ENV['password']
 
   def index
-    #@categories = Category.order(id: :desc).all
+    @categories = Category.order(id: :desc).all
   end
 
   def new
@@ -24,12 +24,7 @@ class Admin::CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(
-      :name,
-      :description,
-      :category_id,
-      :quantity,
-      :image,
-      :price
+      :name
     )
   end
 
