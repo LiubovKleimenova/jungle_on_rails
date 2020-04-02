91f178348eb284e3dbb44346f567306c00fdf6ba
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-04-02 18:18:27 -0400

    updated routes to include sessions

--- config/routes.rb
+++ config/routes.rb
@@ -76,6 +76,10 @@ Rails.application.routes.draw do
   
   # These routes will be for signup. The first renders a form in the browse, the second will 
   # receive the form and create a user in our database using the data given to us by the user.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 end
