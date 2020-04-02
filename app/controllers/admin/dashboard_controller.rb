02f72d84ca872d434713b66a774e49d553472c72
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-04-02 10:14:01 -0400

    fetched data from db

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,19 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @products = Product.count
+    @catagories = Category.count
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :description,
+      :category_id,
+      :quantity,
+      :image,
+      :price
+    )
   end
 end
