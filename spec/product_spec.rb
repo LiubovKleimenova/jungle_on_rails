426bebd2aaba8ebb6660e189f568cdb889fa22bc
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-04-06 17:16:27 -0400

    added validation to category and price

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -21,10 +21,22 @@ RSpec.describe Product, type: :model do
       expect(@product.errors.full_messages[0]).to eq "Name can't be blank"
     end
 
-    it 'ensures that it throws an error Quantity cant be blank without a qantity' do
+    it 'ensures that it throws an error Quantity cant be blank without a quantity' do
       @category = Category.create(name: 'Apparel')
       @product = Product.create(name: 'backpack', category: @category, quantity: nil, price: 200)
       expect(@product.errors.full_messages[0]).to eq "Quantity can't be blank"
+    end
+
+    it 'ensures that it throws an error Category cant be blank without a category' do
+      #@category = Category.create(name: 'Apparel')
+      @product = Product.create(name: 'backpack', category: nil, quantity: 2, price: 200)
+      expect(@product.errors.full_messages[0]).to eq "Category can't be blank"
+    end
+
+    it 'ensures that it throws an error Price is not a number if it is not' do
+      @category = Category.create(name: 'Apparel')
+      @product = Product.create(name: 'backpack', category: @category, quantity: 2, price: nil)
+      expect(@product.errors.full_messages[0]).to eq "Price cents is not a number"
     end
 
 
